e39e642b02c796a6642cb501042d14ea59e44069
ONELua Team <oneluaproject@gmail.com>
2024-11-24 15:00:40 -0600

    Add files via upload

--- webs.lua
+++ webs.lua
@@ -1,6 +1,7 @@
 webs = {
-{ name = 'ANIMEKB', url = "animekb.net" },
-{ name = 'ANIMEYT', url = "animeyt.pro" },
-{ name = 'TIOANIME', url = "tioanime.com" },
+{ name = 'ANIMEKB',   url = "animekb.net"   },
+{ name = 'ANIMEYT',   url = "animeyt.pro"   },
+{ name = 'TIOANIME',  url = "tioanime.com"  },
+{ name = 'TIOHENTAI', url = "tiohentai.com" },
 }
 
